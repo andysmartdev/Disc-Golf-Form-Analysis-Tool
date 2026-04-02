fd253d80b20359de3ca0fef97ea800c3df59943d
Andy Smart <asmart@vertafore.com>
2026-04-02 10:56:46 -0600

    vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   // On GitHub Actions GITHUB_ACTIONS=true is set automatically.
   // Project pages are served at /<repo-name>/ so we need that prefix in CI,
   // but '/' works fine locally.
-  base: process.env.GITHUB_ACTIONS ? '/dg-video-scrubber/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/Disc-Golf-Form-Analysis-Tool/' : '/',
 })
